e5e773b971aa0c47495ce444a81d77e2b03bd4cb
robert-virtual <robertocastill63@gmail.com>
2022-02-18 21:33:59 -0600

    first commit

---
+++ js/record.js
@@ -0,0 +1,84 @@
+const recording = document.getElementById("recording");
+const video = document.getElementById("video");
+const videoC = document.getElementById("video-c");
+const grabar = document.getElementById("grabar");
+const detener = document.getElementById("detener");
+const guardar = document.getElementById("guardar");
+const cancelar = document.getElementById("cancelar");
+const guardarPanel = document.getElementById("save-panel");
+let mediaStream;
+let recordedBlob;
+let getData = () => Promise();
+
+(async () => {
+  mediaStream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+  video.muted = true;
+  video.srcObject = mediaStream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+    videoC.classList.remove("d-none");
+  });
+})();
+
+grabar.addEventListener("click", () => {
+  grabar.classList.add("d-none");
+  detener.classList.remove("d-none");
+});
+
+detener.addEventListener("click", () => {
+  detener.classList.add("d-none");
+  guardarPanel.classList.remove("d-none");
+});
+
+cancelar.addEventListener("click", () => {
+  guardarPanel.classList.add("d-none");
+});
+
+detener.addEventListener("click", async () => {
+  let data = await getData();
+  recordedBlob = new Blob(data, { type: "video/webm" });
+
+  recording.src = URL.createObjectURL(recordedBlob);
+
+  guardar.href = recording.src;
+  guardar.download = "RecordedVideo.webm";
+});
+
+guardar.addEventListener("click", () => {
+  guardarPanel.classList.add("d-none");
+  grabar.classList.remove("d-none");
+});
+
+grabar.addEventListener("click", async () => {
+  getData = startRecording(mediaStream, 1000 * 60 * 10);
+});
+
+function startRecording(stream, lenghtMs) {
+  let recorder = new MediaRecorder(stream);
+  let data = [];
+
+  recorder.ondataavailable = (e) => data.push(e.data);
+  recorder.start();
+
+  let stopped = new Promise((resolve, reject) => {
+    recorder.onstop = resolve;
+    recorder.onerror = reject;
+  });
+
+  async function getData() {
+    if (recorder.state == "recording") {
+      recorder.stop();
+    }
+    await stopped;
+    return data;
+  }
+
+  return getData;
+}
+
+function stopRecording(stream) {
+  stream.getTracks().forEach((track) => track.stop());
+}
